741b86e0feb8a7d8de3983392c0436a47d80909f
João Pedro <joaopjt@icloud.com>
2023-09-15 14:28:18 -0300

    added b() method values

--- src/index.js
+++ src/index.js
@@ -140,7 +140,7 @@ class Snowflake {
     let _letters = [];
     let result = {
       word,
-      wide: cangjie[''],
+      wide: cangjie['water'],
       web: []
     };
     
@@ -156,19 +156,19 @@ class Snowflake {
     if (n.length >= 2) {
       switch(n[1]) {
         case 'value':
-          result.wide = cangjie[''];
+          result.wide = cangjie['female'];
           break;
 
         case 'function':
-          result.wide = cangjie[''];
+          result.wide = cangjie['corpse'];
           break;
 
         case 'behavior':
-          result.wide = cangjie[''];
+          result.wide = cangjie['moon'];
           break;
 
         case 'result':
-          result.wide = cangjie[''];
+          result.wide = cangjie['day'];
           break;
       }
     }
